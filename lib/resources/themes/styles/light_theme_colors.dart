9d4f83cb9d19e2ff6e528851f0af3751944c39b4
Francis Fueconcillo <francisfueconcillo@gmail.com>
2024-04-07 12:09:42 +0800

    Update app icons and theme color

--- lib/resources/themes/styles/light_theme_colors.dart
+++ lib/resources/themes/styles/light_theme_colors.dart
@@ -12,7 +12,7 @@ class LightThemeColors implements ColorStyles {
   @override
   Color get primaryContent => const Color(0xFF000000);
   @override
-  Color get primaryAccent => const Color(0xFF0045a0);
+  Color get primaryAccent => const Color(0xFFFF4800);
 
   @override
   Color get surfaceBackground => Colors.white;
@@ -21,23 +21,23 @@ class LightThemeColors implements ColorStyles {
 
   // app bar
   @override
-  Color get appBarBackground => Colors.blue;
+  Color get appBarBackground => Colors.deepOrange;
   @override
   Color get appBarPrimaryContent => Colors.white;
 
   // buttons
   @override
-  Color get buttonBackground => Colors.blueAccent;
+  Color get buttonBackground => Colors.deepOrange;
   @override
   Color get buttonPrimaryContent => Colors.white;
 
   // bottom tab bar
   @override
-  Color get bottomTabBarBackground => Colors.white;
+  Color get bottomTabBarBackground => const Color.fromRGBO(255, 255, 255, 1);
 
   // bottom tab bar - icons
   @override
-  Color get bottomTabBarIconSelected => Colors.blue;
+  Color get bottomTabBarIconSelected => Colors.deepOrange;
   @override
   Color get bottomTabBarIconUnselected => Colors.black54;
 
